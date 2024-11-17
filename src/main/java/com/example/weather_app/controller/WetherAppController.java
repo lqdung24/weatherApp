a44d476adb9850ff671ae104d705997fd7ced0c3
Dung le <dunglevpr@gmail.com>
2024-11-17 11:31:54 +0700

    hehe

---
+++ src/main/java/com/example/weather_app/controller/WetherAppController.java
@@ -0,0 +1,46 @@
+package com.example.weather_app.controller;
+
+import com.example.weather_app.model.WeatherResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class WetherAppController {
+    @Value("${api.key}")
+    private String apiKey;
+
+    @GetMapping("/")
+    public String getIndex(){
+        return "index";
+    }
+
+    @GetMapping("/weather")
+    public String getWeather(@RequestParam("city") String city, Model model){
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            WeatherResponse weatherResponse = restTemplate.getForObject(url, WeatherResponse.class);
+
+            if (weatherResponse != null) {
+                model.addAttribute("city", weatherResponse.getName());
+                model.addAttribute("country", weatherResponse.getSys().getCountry());
+                model.addAttribute("weatherDescription", weatherResponse.getWeather().get(0).getDescription());
+                model.addAttribute("temperature", weatherResponse.getMain().getTemp());
+                model.addAttribute("humidity", weatherResponse.getMain().getHumidity());
+                model.addAttribute("windSpeed", weatherResponse.getWind().getSpeed());
+                String weatherIcon = "wi wi-owm" + weatherResponse.getWeather().get(0).getId();
+                model.addAttribute("weatherIcon", weatherIcon);
+            }
+        } catch (HttpClientErrorException.NotFound e) {
+            model.addAttribute("error", "City not found!!!");
+        } catch (Exception e) {
+            model.addAttribute("error", "An unexpected error occurred. Please try again later.");
+        }
+        return "weather";
+    }
+}
